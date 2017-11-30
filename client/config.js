721d3b1736a03a328718fa86b160d369112372a9
戈志刚 <gezhigang1005@163.com>
2017-11-30 21:05:21 +0800

    init

---
+++ client/config.js
@@ -0,0 +1,30 @@
+/**
+ * 小程序配置文件
+ */
+
+// 此处主机域名修改成腾讯云解决方案分配的域名
+var host = 'https://kmdtewgk.qcloud.la';
+
+var config = {
+
+    // 下面的地址配合云端 Demo 工作
+    service: {
+        host,
+
+        // 登录地址，用于建立会话
+        loginUrl: `${host}/weapp/login`,
+
+        // 测试的请求地址，用于测试会话
+        requestUrl: `${host}/weapp/user`,
+
+        // 测试的信道服务地址
+        tunnelUrl: `${host}/weapp/tunnel`,
+
+        // 上传图片接口
+        uploadUrl: `${host}/weapp/upload`,
+
+        getList: 'http://192.168.220.105:7080/server/getList.action'
+    }
+};
+
+module.exports = config;
---
+++ client/pages/index/index.js
@@ -0,0 +1,109 @@
+//index.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+
+Page({
+    data: {
+        userInfo: {},
+        logged: false,
+        takeSession: false,
+        requestResult: ''
+    },
+
+    nextNode(){
+        wx.showToast({
+            title: '已发送验证码',
+            icon: 'success',
+            duration: 2000000,
+            success(){
+                setTimeout(()=>{
+                    wx.navigateTo({
+                        url: '../verification/index'
+                    })
+                },2000)
+            }
+        })
+        
+    },
+
+    // 用户登录示例
+    login: function() {
+        if (this.data.logged) return
+
+        util.showBusy('正在登录')
+        var that = this
+
+        // 调用登录接口
+        qcloud.login({
+            success(result) {
+                console.log(result)
+                if (result) {
+                    util.showSuccess('登录成功');
+                    console.log(result)
+                    that.setData({
+                        userInfo: result,
+                        logged: true
+                    })
+                } else {
+                    // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
+                    qcloud.request({
+                        url: config.service.loginUrl,
+                        login: true,
+                        success(result) {
+                            util.showSuccess('登录成功');
+                            console.log(result)
+                            that.setData({
+                                userInfo: result.data.data,
+                                logged: true
+                            })
+                        },
+
+                        fail(error) {
+                            util.showModel('请求失败', error)
+                            console.log('request fail', error)
+                        }
+                    })
+                }
+            },
+
+            fail(error) {
+                util.showModel('登录失败', error)
+                console.log('登录失败', error)
+            }
+        })
+    },
+
+    // 切换是否带有登录态
+    switchRequestMode: function (e) {
+        this.setData({
+            takeSession: e.detail.value
+        })
+        this.doRequest()
+    },
+
+    doRequest: function () {
+        util.showBusy('请求中...')
+        var that = this
+        var options = {
+            url: config.service.requestUrl,
+            login: true,
+            success (result) {
+                util.showSuccess('请求成功完成')
+                console.log('request success', result)
+                that.setData({
+                    requestResult: JSON.stringify(result.data)
+                })
+            },
+            fail (error) {
+                util.showModel('请求失败', error);
+                console.log('request fail', error);
+            }
+        }
+        if (this.data.takeSession) {  // 使用 qcloud.request 带登录态登录
+            qcloud.request(options)
+        } else {    // 使用 wx.request 则不带登录态
+            wx.request(options)
+        }
+    }
+})
